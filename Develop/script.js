42e4c29b30a62707585ab5727dcc9ed646056ce9
Rachel <rachelmmarriott@gmail.com>
2022-01-05 22:11:18 -0700

    added prompts for password length and character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
-
+function generatePassword() {
+  var passwordLength = window.prompt("Please choose a password length between 8 - 128 characters.");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a password between 8 - 128 characters.");
+    window.prompt("Please choose a password length between 8 - 128 characters.");
+  }
+  var characterTypes = window.prompt("Please indicate which character types to include: lowercase, uppercase, numeric, and/or special characters?");
+  window.confirm("You have chosen a password length of " + passwordLength + " and the following character type(s): " + characterTypes+ ". Is this correct?");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
